96493a85357f5be43a5ec8fa3569e95e21590787
wkdtmddus <jangsy1207@gmail.com>
2023-07-31 18:08:20 +0900

    feat : user/signup, login, logout, jwt, cors, security

--- src/main/java/com/sparta/toogo/domain/user/controller/UserController.java
+++ src/main/java/com/sparta/toogo/domain/user/controller/UserController.java
@@ -1,4 +1,36 @@
 package com.sparta.toogo.domain.user.controller;
 
+import com.sparta.toogo.domain.user.dto.UserRequestDto;
+import com.sparta.toogo.domain.user.dto.UserResponseDto;
+import com.sparta.toogo.domain.user.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
 public class UserController {
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public UserResponseDto signUp(@Valid @RequestBody UserRequestDto userRequestDto) {
+        return userService.signUp(userRequestDto);
+    }
+
+    @PostMapping("/email")
+    public Boolean checkeMail(@RequestParam String email) {
+        return !userService.checkEmail(email);
+    }
+
+    @PostMapping("/nickname")
+    public Boolean checkNickname(@RequestParam String nickname) {
+        return !userService.checkNickname(nickname);
+    }
+
+    @PostMapping("/logout")
+    public UserResponseDto logOut(HttpServletRequest request) {
+        return userService.logOut(request);
+    }
 }
--- src/main/java/com/sparta/toogo/domain/user/dto/UserResponseDto.java
+++ src/main/java/com/sparta/toogo/domain/user/dto/UserResponseDto.java
@@ -1,4 +1,11 @@
 package com.sparta.toogo.domain.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UserResponseDto {
+    private String msg;
+    private int status;
 }
