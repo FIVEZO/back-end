a669b31d45ed866792c9489a45543937319657d8
wkdtmddus <jangsy1207@gmail.com>
2023-08-04 20:49:08 +0900

    fix : email 수정

--- src/main/java/com/sparta/toogo/global/email/service/EmailService.java
+++ src/main/java/com/sparta/toogo/global/email/service/EmailService.java
@@ -43,21 +43,21 @@ public class EmailService {
         message.addRecipients(MimeMessage.RecipientType.TO, email);//보내는 대상
         message.setSubject(ADMIN_NAME + " 이메일 인증");//제목
 
-        String msgg = "";
-        msgg += "<div style='margin:20px;'>";
-        msgg += "<h1> 안녕하세요 " + ADMIN_NAME + "입니다. </h1>";
-        msgg += "<br>";
-        msgg += "<p>아래 코드를 복사해 입력해주세요<p>";
-        msgg += "<br>";
-        msgg += "<p>감사합니다.<p>";
-        msgg += "<br>";
-        msgg += "<div align='center' style='border:1px solid black; font-family:verdana';>";
-        msgg += "<h3 style='color:green;'>회원가입 인증 코드입니다.</h3>";
-        msgg += "<div style='font-size:130%'>";
-        msgg += "CODE : <strong>";
-        msgg += code + "</strong><div><br/> ";
-        msgg += "</div>";
-        message.setText(msgg, "utf-8", "html");//내용
+        String msg = "";
+        msg += "<div style='margin:20px;'>";
+        msg += "<h1> 안녕하세요 " + ADMIN_NAME + "입니다. </h1>";
+        msg += "<br>";
+        msg += "<p>아래 코드를 복사해 입력해주세요<p>";
+        msg += "<br>";
+        msg += "<p>감사합니다.<p>";
+        msg += "<br>";
+        msg += "<div align='center' style='border:1px solid black; font-family:verdana';>";
+        msg += "<h3 style='color:green;'>회원가입 인증 코드입니다.</h3>";
+        msg += "<div style='font-size:130%'>";
+        msg += "CODE : <strong>";
+        msg += code + "</strong><div><br/> ";
+        msg += "</div>";
+        message.setText(msg, "utf-8", "html");//내용
         message.setFrom(new InternetAddress(ADMIN_EMAIL, ADMIN_NAME)); // 보내는 사람
 
         return message;
@@ -67,17 +67,12 @@ public class EmailService {
     public static String createKey() {
         StringBuilder key = new StringBuilder();
         Random rnd = new Random();
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-        for (int i = 0; i < 8; i++) { // 인증코드 8자리
-            int index = rnd.nextInt(3); // 0~2 까지 랜덤
-            switch (index) {
-                //  a~z  (ex. 1+97=98 => (char)98 = 'b')
-                case 0 -> key.append((char) ((int) (rnd.nextInt(26)) + 97));
-                //  A~Z
-                case 1 -> key.append((char) ((int) (rnd.nextInt(26)) + 65));
-                // 0~9
-                case 2 -> key.append((rnd.nextInt(10)));
-            }
+        for (int i = 0; i < 8; i++) {
+            int index = rnd.nextInt(characters.length());
+            char randomChar = characters.charAt(index);
+            key.append(randomChar);
         }
         return key.toString();
     }
