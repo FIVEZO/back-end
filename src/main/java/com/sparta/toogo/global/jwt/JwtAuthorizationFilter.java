ca6f3ee75e6b66a65eee76344a253cb0cb109cd5
wkdtmddus <jangsy1207@gmail.com>
2023-08-20 08:23:30 +0900

    fix : docker test

--- src/main/java/com/sparta/toogo/global/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/sparta/toogo/global/jwt/JwtAuthorizationFilter.java
@@ -43,6 +43,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                     // 유효한 refreshToken인지 검증
                     if (jwtUtil.validateRegenerate(accessTokenValue, refreshTokenValue)) {
                         jwtUtil.regenerateToken(refreshTokenValue, res);
+
                         log.info("새로운 AccessToken, RefreshToken 발급 완료");
                         res.getWriter().write("새로운 AccessToken, RefreshToken이 발급되었습니다.");
                     } else {
