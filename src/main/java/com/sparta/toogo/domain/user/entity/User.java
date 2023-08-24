cc00758acf94d4f8e8db75d01341f29d4acd0f7a
wkdtmddus <jangsy1207@gmail.com>
2023-08-24 12:33:24 +0900

    fix : 로그인 수정

--- src/main/java/com/sparta/toogo/domain/user/entity/User.java
+++ src/main/java/com/sparta/toogo/domain/user/entity/User.java
@@ -24,7 +24,7 @@ public class User extends Timestamped {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String email;
 
     @Column(nullable = false)
