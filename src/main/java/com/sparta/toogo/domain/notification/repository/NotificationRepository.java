ee91fa411914293269ddb9f62cb89b115cd73b75
wkdtmddus <jangsy1207@gmail.com>
2023-08-28 10:07:25 +0900

    fix : notification 수정

--- src/main/java/com/sparta/toogo/domain/notification/repository/NotificationRepository.java
+++ src/main/java/com/sparta/toogo/domain/notification/repository/NotificationRepository.java
@@ -3,6 +3,7 @@ package com.sparta.toogo.domain.notification.repository;
 import com.sparta.toogo.domain.notification.entity.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -10,4 +11,6 @@ public interface NotificationRepository extends JpaRepository<Notification, Long
     Optional<Notification> findById(Long id);
 
     List<Notification> findByUserId(Long id);
+
+    List<Notification> findByCreatedAtBefore(LocalDate twoDaysAgo);
 }
