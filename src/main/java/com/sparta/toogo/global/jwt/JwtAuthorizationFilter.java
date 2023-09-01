593f925a1bb6c2701cc6e02f072dee7498d36399
wkdtmddus <jangsy1207@gmail.com>
2023-09-01 09:52:26 +0900

    test

--- src/main/java/com/sparta/toogo/global/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/sparta/toogo/global/jwt/JwtAuthorizationFilter.java
@@ -9,6 +9,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
@@ -53,7 +54,7 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
                         throw new UnauthorizedException(ErrorCode.REGENERATED_TOKEN);
                     } catch (UnauthorizedException e) {
                         log.error("토큰 재발급");
-                        res.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                        res.setStatus(HttpStatus.I_AM_A_TEAPOT.value());
                         return;
                     }
                 }
